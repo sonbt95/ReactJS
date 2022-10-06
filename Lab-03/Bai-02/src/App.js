b33cc908d4fd82f16ff4d64420bccf11fb9bd7b9
sonbt95 <buithienson95@gmail.com>
2022-10-06 19:22:46 +0700

    Do lab 03

---
+++ Lab-03/Bai-02/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CountdownTime from "./Components/CountdownTime/CountdownTime";
+
+function App() {
+  return (
+    <div className="App">
+      <CountdownTime />
+    </div>
+  );
+}
+
+export default App;
